78acde720b58012e4b8231da3b75016f7ec81c91
Ahmed <ahmedkiller0234@gmail.com>
2025-12-18 15:19:15 +0500

    Shadcn Setup, Themes, UI Components Shadcn Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@/components/ui/button'
+import { ModeToggle } from '@/components/ui/Themetoggler'
+
 const page = () => {
   return (
     <div className="border borer">
       <h1 className="text-6xl text-center font-sans text-rose-500">Main Page 12!</h1>
       <h1 className="text-6xl text-center font-mono text-teal-500">Main Page 12!</h1>
       <h1 className="text-6xl text-center font-bartel text-yellow-500">Main Page 12!</h1>
+      <Button>Hello World</Button>
     </div>
   )
 }
