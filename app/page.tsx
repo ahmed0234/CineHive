c758583f92ae99bc52241022bb05bd67a1464f07
Ahmed <ahmedkiller0234@gmail.com>
2025-12-18 14:46:16 +0500

    Bartel Font Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const page = () => {
   return (
-    <div>
-      <h1>Main Page !</h1>
+    <div className="border borer">
+      <h1 className="text-6xl text-center font-sans text-rose-500">Main Page 12!</h1>
+      <h1 className="text-6xl text-center font-mono text-teal-500">Main Page 12!</h1>
+      <h1 className="text-6xl text-center font-bartel text-yellow-500">Main Page 12!</h1>
     </div>
   )
 }
