42ef23876bf275bce0e50a6e40379dc3efce5659
Ahmed <ahmedkiller0234@gmail.com>
2025-12-26 21:17:13 +0500

    finallizing last things

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,77 @@
-import Footer from '@/components/footer'
-import { MovieHeroCarousel } from '@/components/herocarousel'
-import { MovieRow } from '@/components/movie-row'
-import { Navbar } from '@/components/navbar'
+import Footer from '@/components/footer';
+import { MovieHeroCarousel } from '@/components/herocarousel';
+import { MovieRow } from '@/components/movie-row';
+import { Navbar } from '@/components/navbar';
 
 const fetchMovies = async () => {
-  const apiKey = process.env.MOVIE_DB_API_KEY
+  const apiKey = process.env.MOVIE_DB_API_KEY;
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`
-  )
-  const data = await res.json()
-  return data.results
-}
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`,
+    { next: { revalidate: 12 * 60 * 60 } }
+  );
+  const data = await res.json();
+  return data.results;
+};
 
 const UpcomingMovies = async () => {
-  const apiKey = process.env.MOVIE_DB_API_KEY
+  const apiKey = process.env.MOVIE_DB_API_KEY;
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`
-  )
-  const data = await res.json()
-  return data.results
-}
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`,
+    { next: { revalidate: 12 * 60 * 60 } }
+  );
+  const data = await res.json();
+  return data.results;
+};
 
 const popularMovies = async () => {
-  const apiKey = process.env.MOVIE_DB_API_KEY
-  const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=1`)
-  const data = await res.json()
-  return data.results
-}
+  const apiKey = process.env.MOVIE_DB_API_KEY;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=1`,
+    { next: { revalidate: 12 * 60 * 60 } }
+  );
+  const data = await res.json();
+  return data.results;
+};
 
 const scififantasies = async () => {
-  const apiKey = process.env.MOVIE_DB_API_KEY
+  const apiKey = process.env.MOVIE_DB_API_KEY;
   const res = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=878,14&sort_by=popularity.desc&page=1`
-  )
-  const data = await res.json()
-  return data.results
-}
+    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=878,14&sort_by=popularity.desc&page=1`,
+    { next: { revalidate: 12 * 60 * 60 } }
+  );
+  const data = await res.json();
+  return data.results;
+};
 
 const romanceDrama = async () => {
-  const apiKey = process.env.MOVIE_DB_API_KEY
+  const apiKey = process.env.MOVIE_DB_API_KEY;
   const res = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=10749,18&sort_by=popularity.desc&page=1`
-  )
-  const data = await res.json()
-  return data.results
-}
+    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=10749,18&sort_by=popularity.desc&page=1`,
+    { next: { revalidate: 12 * 60 * 60 } }
+  );
+  const data = await res.json();
+  return data.results;
+};
 
 const popularSeasons = async () => {
-  const apiKey = process.env.MOVIE_DB_API_KEY
-  const res = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`)
-  const data = await res.json()
-  return data.results
-}
+  const apiKey = process.env.MOVIE_DB_API_KEY;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`,
+    { next: { revalidate: 12 * 60 * 60 } }
+  );
+  const data = await res.json();
+  return data.results;
+};
 
 const horror = async () => {
-  const apiKey = process.env.MOVIE_DB_API_KEY
+  const apiKey = process.env.MOVIE_DB_API_KEY;
   const res = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=27,53&sort_by=popularity.desc&page=1`
-  )
-  const data = await res.json()
-  return data.results
-}
+    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=27,53&sort_by=popularity.desc&page=1`,
+    { next: { revalidate: 12 * 60 * 60 } }
+  );
+  const data = await res.json();
+  return data.results;
+};
 
 const page = async () => {
   const [
@@ -79,7 +90,7 @@ const page = async () => {
     romanceDrama(),
     horror(),
     popularSeasons(),
-  ])
+  ]);
   return (
     <div className="">
       <Navbar />
@@ -94,7 +105,7 @@ const page = async () => {
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
